04ef7bf92a50597fad8f1e95332d496b8e99c686
zshidlab <154736226+zshidlab@users.noreply.github.com>
2025-04-08 20:29:43 +0800

    Update ShoppingCartPage.vue

--- vue_work/online_shop/src/views/ShoppingCartPage.vue
+++ vue_work/online_shop/src/views/ShoppingCartPage.vue
@@ -1,31 +1,37 @@
 <script setup>
     import { useAuthStore } from '../stores/auth';
-// 模拟数据
-const mockProducts = [
-    { product_id: 1, product_name: "商品1", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-    { product_id: 2, product_name: "商品2", price: 199.99, stock: 5 ,img: null},
-    { product_id: 3, product_name: "商品3", price: 99.99, stock: 10 ,img: "/src/assets/vue.svg"},
-    { product_id: 4, product_name: "商品4", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-    { product_id: 5, product_name: "商品5", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-    { product_id: 6, product_name: "商品6", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-    { product_id: 7, product_name: "商品7", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-    { product_id: 8, product_name: "商品8", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-    { product_id: 9, product_name: "商品9", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-    { product_id: 10, product_name: "商品10", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-    { product_id: 11, product_name: "商品11", price: 99.99, stock: 10, img: "/src/assets/commodity.png" },
-    { product_id: 12, product_name: "商品12", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
-];
-const authStore = useAuthStore()
+    import { onMounted } from 'vue';
+    import { useRouter } from 'vue-router';
+    const router = useRouter()
+    // 模拟数据
+    const mockProducts = [
+        { product_id: 1, product_name: "商品1", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+        { product_id: 2, product_name: "商品2", price: 199.99, stock: 5 ,img: null},
+        { product_id: 3, product_name: "商品3", price: 99.99, stock: 10 ,img: "/src/assets/vue.svg"},
+        { product_id: 4, product_name: "商品4", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+        { product_id: 5, product_name: "商品5", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+        { product_id: 6, product_name: "商品6", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+        { product_id: 7, product_name: "商品7", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+        { product_id: 8, product_name: "商品8", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+        { product_id: 9, product_name: "商品9", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+        { product_id: 10, product_name: "商品10", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+        { product_id: 11, product_name: "商品11", price: 99.99, stock: 10, img: "/src/assets/commodity.png" },
+        { product_id: 12, product_name: "商品12", price: 99.99, stock: 10 ,img: "/src/assets/commodity.png"},
+    ];
+    const authStore = useAuthStore()
 
-function notLoggedAlert() {//弹窗登录警告函数
-    alert("未登录，将跳转至登录界面!");
-}
-function buy() {//商品结算函数
-    
-}
-function selected() {//选择该商品函数
-    
-}
+    onMounted(() => {//弹窗登录警告函数
+        if(!authStore.isLogged){
+            alert("未登录，将跳转至登录界面!")
+            router.push('/login')
+        }  
+    }) 
+    function buy() {//商品结算函数
+        
+    }
+    function selected() {//选择该商品函数
+        
+    }
 </script>
 
 <template>

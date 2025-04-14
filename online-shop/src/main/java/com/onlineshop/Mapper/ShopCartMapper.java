eac1d96955865d9157253edb74a024b838ff78ad
贾尔亚 <147225073+2287635182@users.noreply.github.com>
2025-04-14 17:29:07 +0800

    11

--- online-shop/src/main/java/com/onlineshop/Mapper/ShopCartMapper.java
+++ online-shop/src/main/java/com/onlineshop/Mapper/ShopCartMapper.java
@@ -1,5 +1,6 @@
 package com.onlineshop.Mapper;
 import com.onlineshop.entity.Goods;
+import com.onlineshop.entity.ShopCartGoods;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,5 +11,6 @@ public interface ShopCartMapper {
     List<Goods> searchByPage(Long userId, int begin, int end);
     void delete(Long goodsId);
 
-    void add(Goods goods,Long userId);
+
+    void add(ShopCartGoods shopCartGoods);
 }
---
+++ online-shop/src/main/java/com/onlineshop/entity/ShopCartGoods.java
@@ -0,0 +1,40 @@
+package com.onlineshop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShopCartGoods {
+    //商品ID
+    private Long goodsId;
+    //商品名称
+    private String name;
+    //商品描述
+    private String description;
+    //商品价格
+    private Double price;
+    //商品库存
+    private Integer stock;
+    //商品图片
+    private String image;
+    //商品类别
+    private String category;
+    //创建时间
+    private LocalDateTime createTime;
+    //更新时间
+    private LocalDateTime updateTime;
+    //版本号
+    private String version;
+    //商户ID
+    private Long productId;
+    //用户id
+    private Long userId;
+}
